f93bc6f5f76464d968a92393c458724d46e3c187
iriscorn <korniienko.iris@gmail.com>
2023-10-17 19:51:08 +0300

    created html and js files

---
+++ main.js
@@ -0,0 +1,3 @@
+let userName = prompt('Hi! How can I call you?', 'Because I would call you mine)');
+alert('Hello, ' + userName + '! How are you?');
+document.write('Congrats, ' + userName + '!! You are the first person visiting this "website"!');
